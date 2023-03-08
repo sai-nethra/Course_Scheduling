21fa5042c74d20ba40f3455b0813190f31139b63
sai-nethra-meesho <manikonda.nethra@meesho.com>
2023-03-08 12:52:05 +0530

    Unit tests

--- src/test/java/com/example/geektrust/MainTest.java
+++ src/test/java/com/example/geektrust/MainTest.java
@@ -1,4 +1,25 @@
 package com.example.geektrust;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MainTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp(){
+        System.setOut(new PrintStream(outContent));
+    }
+    @Test
+    public void testMain(){
+        CourseScheduling.main(new String[]{
+                "sample_input/input1.txt"
+        });
+        assertTrue(!outContent.toString().trim().isEmpty());
+    }
 }
